47c3130ee1441eb7fccc7fba2f0de9405f238304
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-07-03 07:17:39 +0200

    Login - Obtener id de usuario autenticado, pero usando Custom Annotation

--- src/main/java/com/jmunoz/tddautenticacion/user/LoginController.java
+++ src/main/java/com/jmunoz/tddautenticacion/user/LoginController.java
@@ -1,5 +1,6 @@
 package com.jmunoz.tddautenticacion.user;
 
+import com.jmunoz.tddautenticacion.shared.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +16,7 @@ public class LoginController {
     UserService userService;
 
     @PostMapping("/api/1.0/login")
-    Map<String, Object> handleLogin(Authentication authentication) {
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+    Map<String, Object> handleLogin(@CurrentUser CustomUserDetails customUserDetails) {
         User user = userService.findByUsername(customUserDetails.getUsername());
 
         return Collections.singletonMap("id", user.getId());
